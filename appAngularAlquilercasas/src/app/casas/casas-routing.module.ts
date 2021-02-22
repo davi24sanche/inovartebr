07ece6e9f24ec5b617e37d88b923fa2467f4fd2c
JOSE DAVID SANCHEZ CISNERO <jsanchezc@detecemp.com>
2021-02-21 22:30:43 -0600

    Grid de Productos y Transición

--- appAngularAlquilercasas/src/app/casas/casas-routing.module.ts
+++ appAngularAlquilercasas/src/app/casas/casas-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoComponent } from './producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'casas/producto', component: ProductoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- appAngularAlquilercasas/src/app/casas/casas.module.ts
+++ appAngularAlquilercasas/src/app/casas/casas.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CasasRoutingModule } from './casas-routing.module';
+import { ProductoComponent } from './producto/producto.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductoComponent],
   imports: [
     CommonModule,
     CasasRoutingModule
