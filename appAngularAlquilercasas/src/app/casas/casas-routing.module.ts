be57b5012a8b2444bd620e5cf5c30e5b9c85b917
JOSE DAVID SANCHEZ CISNERO <jsanchezc@detecemp.com>
2021-03-08 20:03:49 -0600

    Reserva, productos y detalle

--- appAngularAlquilercasas/src/app/casas/casas-routing.module.ts
+++ appAngularAlquilercasas/src/app/casas/casas-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductoComponent } from './producto/producto.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 const routes: Routes = [
-
-  { path: 'casas/producto', component: ProductoComponent},
+  { path: 'casas/producto', component: ProductoComponent },
+  { path: 'casas/reserva', component: ReservaComponent },
+  { path: 'casas/detalle', component: DetalleComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CasasRoutingModule { }
+export class CasasRoutingModule {}
--- appAngularAlquilercasas/src/app/casas/casas.module.ts
+++ appAngularAlquilercasas/src/app/casas/casas.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CasasRoutingModule } from './casas-routing.module';
 import { ProductoComponent } from './producto/producto.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { DetalleComponent } from './detalle/detalle.component';
 
 
 @NgModule({
-  declarations: [ProductoComponent],
+  declarations: [ProductoComponent, ReservaComponent, DetalleComponent],
   imports: [
     CommonModule,
     CasasRoutingModule
